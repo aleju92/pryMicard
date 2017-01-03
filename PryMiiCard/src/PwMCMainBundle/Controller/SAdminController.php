250838c8cb18597586723181658214a649ef77e1
AngeloDAvid <angelodavid95@hotmail.es>
2017-01-03 00:40:06 -0500

    Creacion del modelo MCV de los administadores

--- PryMiiCard/src/PwMCMainBundle/Controller/SAdminController.php
+++ PryMiiCard/src/PwMCMainBundle/Controller/SAdminController.php
@@ -4,6 +4,7 @@ namespace PwMCMainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use PwMCMainBundle\Entity\Administrador;
 
 class SAdminController extends Controller
 {
@@ -15,15 +16,15 @@ class SAdminController extends Controller
     public function pageselectAction($page)
     {
     	if($page =="categorias" || $page=="admins"){
-    		return $this->render("PwMCMainBundle:sa:".$page.".html.twig",array("menssaje"=>' '));
+    		if($page=="categorias"){
+    			return  $this->redirect($this->generateUrl("pw_mc_main_catgetall"));
+    		}else{
+    			return  $this->redirect($this->generateUrl("pw_mc_main_admins"));
+    			//return $this->render("PwMCMainBundle:sa:".$page.".html.twig",array("menssaje"=>' '));
+    		}    		
     	}else{
     		return  $this->redirect($this->generateUrl("pw_mc_main_adminp"));
     	}
     	
-    }
-    
-    public function ayudaAction()
-    {
-    	return new Response("<html><body>hola</body></html>");
     }
 }
--- PryMiiCard/src/PwMCMainBundle/Controller/categoriasController.php
+++ PryMiiCard/src/PwMCMainBundle/Controller/categoriasController.php
@@ -5,6 +5,7 @@ namespace PwMCMainBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use PwMCMainBundle\Entity\Categoria;
 use Symfony\Component\HttpFoundation\Response;
+//use PwMCMainBundle\Form\CategoriaType;
 
 class categoriasController extends Controller
 {
@@ -13,17 +14,21 @@ class categoriasController extends Controller
     	$Categoria->setDesCat($descripcion);
     	$Categoria->setEstado(1);
     	
+    	$categorias= $this->catAll();
     	$em=$this->getDoctrine()->getManager();
     	$em->persist($Categoria);
     	$em->flush();
-    	
+    	//$form=$this->createForm(new CategoriaType(),$Categoria);
+    	//$form->handleRequest($request);
     	$ms="La categoria ".$descripcion." ha sido ingresada con exito";
-    	return $this->render("PwMCMainBundle:sa:categorias.html.twig",array("menssaje"=>$ms));
+    	//return $this->render("PwMCMainBundle:sa:formCat.html.twig",array("menssaje"=>$ms,"form",$form->createView()));
+    	return $this->render("PwMCMainBundle:sa:categorias.html.twig",array("menssaje"=>$ms,"categorias"=>$categorias));
     }
     public function editCatAction($id,$descripcion){
-    	if(isset($descripcion)||$descripcion==" "){
+    	/*if(isset($descripcion)||$descripcion==" "){
     		$ms="Descripcion no definida";
-    	}else{
+    	}else{*/
+    	$categorias= $this->catAll();
 	    	$em=$this->getDoctrine()->getManager();
 	    	$categoria=$em->find('PwMCMainBundle:Categoria',$id);
 	    	if(!$categoria){
@@ -33,36 +38,41 @@ class categoriasController extends Controller
 	    		$categoria->setDesCat($descripcion);
 	    		$em->flush();
 	    	}
-    	}
-    	return $this->render("PwMCMainBundle:sa:categorias.html.twig",array("menssaje"=>$ms));    	
+    	//}*/
+    	return $this->render("PwMCMainBundle:sa:categorias.html.twig",array("menssaje"=>$ms,"categorias"=>$categorias));    	
     }
     public function deleteCatAction($id,$estado){
+    	$categorias= $this->catAll();
     	$em=$this->getDoctrine()->getManager();
     	$categoria=$em->find('PwMCMainBundle:Categoria',$id);
     	if(!$categoria){
     		$ms="No existe esa categoria con el registro: ".$id;
     	}else{
     		$ms="Ha sido eliminada la categoria con Código: ".$id;
-	    	$categoria->setDesCat($estado);
+	    	$categoria->setEstado($estado);
 	    	$em->flush();
     	}
-    	return $this->render("PwMCMainBundle:sa:categorias.html.twig",array("menssaje"=>$ms));
+    	return $this->render("PwMCMainBundle:sa:categorias.html.twig",array("menssaje"=>$ms,"categorias"=>$categorias));
     }
     public function getCatsAction(){
-    	$em=$this->getDoctrine()->getManager();
-    	$categorias= $em->getRepository('PwMCMainBundle:Categoria')->findAll();
+    	$categorias= $this->catAll();
     	/*
     	$ms="Categorias<br>";
     	foreach ($categorias as $categoria){
     		$ms.=$categoria->getId()."=".$categoria->getDesCat()."<br>";
     	}
     	//return new Response("<html><body>hola</body></html>");
-    	return new Response($ms);*/
+    	return new Response($ms);*/    	    	
     	return $this->render("PwMCMainBundle:sa:categorias.html.twig",array("menssaje"=>'',"categorias"=>$categorias));
     }
     public function getCatXidAction($id){
     	$em=$this->getDoctrine()->getManager(); 
     	$categoria=$em->find('PwMCMainBundle:Categoria',$id);
     	return new Response ($categoria->getId()."=".$categoria->getDesCat()."<br>");
+    }
+    private function catAll(){
+    	$em=$this->getDoctrine()->getManager();
+    	$categorias= $em->getRepository('PwMCMainBundle:Categoria')->findAll();
+    	return $categorias;
     }
 }
