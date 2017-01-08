95f47b0f2905c37d943cfa3beacf9da59b4e8aec
TatyMaybeth <tattocsb_010994@hotmail.com>
2017-01-07 20:21:32 -0500

    Cambiando rutas usuario

--- PryMiiCard/src/AppBundle/Controller/UserController.php
+++ PryMiiCard/src/AppBundle/Controller/UserController.php
@@ -8,13 +8,12 @@ use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
-	 /**
-	  * @Route("/usuario", name="user_index");
-	  */
+	
 	public function indexAction()
 	{
 		return $this->render('user/index.html.twig');
 	}
+	
 	
 	public function pageselectAction($page)
 	{
