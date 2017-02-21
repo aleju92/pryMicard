638b207376a739d4812bebda4750a4b6a7f89b19
AngeloDavid <angelodavid95@hotmail.es>
2017-02-21 09:06:56 -0500

    Password modificacion

--- PryMiiCard/web/js/verificarPass.js
+++ PryMiiCard/web/js/verificarPass.js
@@ -10,50 +10,27 @@ $(document).ready(function () {
             $.ajax({
                 method:"POST",
                 url:Routing.generate('SaAdmPass'),
-                data:$('#formV').serialize(),
+                data:{pass:$('#formV_PasswordTemp').val() },
                 dataType:'json',
                 success: function (data) {
-                    console.log(data.form);
+                    if(data.typems == "danger"){
+                        var ms= "<div class= 'flash-success alert alert-"+data.typems+"'  role='alert'>";
+                        ms+="<button type='button' class='close' data-dismiss= 'alert' aria-label='Close'>";
+                        ms+="<span aria-hidden='true'>x</span></button>";
+                        ms+=data.mensaje;
+                        ms+="</div>";
+                        $('#mensaje').html(ms); // presento el mensaje
+                        console.log(data.passw);
+                    }else{
+                        $('#modal').html(data.Form);
+                        $('#modalPassword').modal('show');
+                    }
                 },
                 error: function () {
 
                 }
             });
         });
- /*   $(document).on('click','#btnVet',function (e) {
-        e.preventDefault();
-        $.ajax({
-            method:"POST",
-            url:Routing.generate('SaAdmPass'),
-            data:{idUser:5,passText:$('#txtPass').val()},
-            dataType:'json',
-            success: function (data,jqXHR)
-            {
-                if(data.typems == "danger"){
-                    var ms= "<div class= 'flash-success alert alert-"+data.typems+"'  role='alert'>";
-                    ms+="<button type='button' class='close' data-dismiss= 'alert' aria-label='Close'>";
-                    ms+="<span aria-hidden='true'>x</span></button>";
-                    ms+=data.mensaje;
-                    ms+="</div>";
-                    $('#mensaje').html(ms); // presento el mensaje
-                    console.log(data.passw);
-                }else{
-                            $('#modal').html(data.Form);
-                             $('#modalPassword').modal('show');
-
-                }
-            },
-            error: function (jqXHR,textStatus, errorThrown, exception)
-            {
-                var ms= "<div class= 'flash-success alert alert-danger'  role='alert'>";
-                ms+="<button type='button' class='close' data-dismiss= 'alert' aria-label='Close'>";
-                ms+="<span aria-hidden='true'>x</span></button>";
-                ms+="No se puedo hacer conectar al servidor ";
-                ms+="</div>";
-                $('#mensaje').html(ms); // presento el mensaje
-            }
-        });
-    });*/
 
     $(document).on('click','#form_cambiar',function (e) {
         e.preventDefault();
@@ -64,9 +41,26 @@ $(document).ready(function () {
             dataType:'json',
             success: function (data,jqXHR)
             {
-                console.log(data.message);
-                console.log(data.pass);
-                console.log(data.id);
+                var modal;
+                var ico;
+                if(data.typems != "danger"){
+                    $('#modalPassword').modal('hide');
+                    modal="#mensaje";
+                    ico="<i class='fa fa-check fa-2x' aria-hidden='true'></i>";
+                }else{
+                    modal="#MensajeModal";
+                    ico="<i class='fa fa-exclamation-triangle fa-2x' aria-hidden='true'></i>";
+                    $('#form_PasswordTemp_first').val('');
+                    $('#form_PasswordTemp_second').val('');
+                }
+                var ms= "<div class= 'flash-success alert alert-"+data.typems+"'  role='alert'>";
+                ms+="<button type='button' class='close' data-dismiss= 'alert' aria-label='Close'>";
+                ms+="<span aria-hidden='true'>x</span></button>";
+                ms+=ico;
+                ms+=data.mensaje;
+                ms+="</div>";
+                $(modal).html(ms);
+
             },
             error: function(data){}
         });
